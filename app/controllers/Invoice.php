255701807daac8abcb2af7ff20a49b784b1d7957
Syamiel00 <amirunaqim@gmail.com>
2022-06-16 16:07:27 +0800

    add controller invoice and view

---
+++ app/controllers/Invoice.php
@@ -0,0 +1,8 @@
+<?php
+    class Invoice extends Controller{
+        public function index(){
+            $this->view("Invoice/index");
+
+        }
+    }
+        
---
+++ app/views/Invoice/Index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+    <link rel="stylesheet" href="<?=baseurl?>css/style.css">
+    <title>Invoice | GenSto</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="mypic">
+            <img src="<?=baseurl?>img/logo.png" style=" border-radius: 50%;" width="100px" >
+            <h4>ADMIN</h4>
+        </div>
+        <a class="active" href="index.html">Home</a>
+        <a href="photo_gallery.html">Users</a>
+        <a href="hobby.html">Companys</a>
+       <a href="aboutus.html">Logout</a>
+    </div>
+    <div class="container">
+
+<div class="search_wrap search_wrap_3">
+    <div class="search_box">
+        <input type="text" class="input" placeholder="search...">
+        <div class="btn btn_common">
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
+</div>
+
+</div>
+</body>
