8ef40cd2ab31ceb2b322b20b16d7fe8af35ff85a
saifultech <saifultech.official@gmail.com>
2022-06-16 12:42:12 +0800

    update user controller , update user page and create components

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -1,7 +1,15 @@
 <?php
 class User extends Controller{
     public function index(){
+        $this->view('Components/header');
         $this->view('User/index');
+        $this->view('Components/footer');
+    }
+    public function profile(){
+
+    }
+    public function kemaskini(){
+        
     }
 }
         
---
+++ app/views/Components/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=baseurl?>css/style.css">
+    <title>Dashboard | GenSto</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="mypic">
+            <img src="<?=baseurl?>img/logo.png" style=" border-radius: 50%;" width="100px" >
+            <h4>ADMIN</h4>
+        </div>
+        <a href="index.html">Home</a>
+        <a class="active" href="<?=baseurl?>User">Users</a>
+        <a href="hobby.html">Companys</a>
+       <a href="aboutus.html">Logout</a>
+    </div>
