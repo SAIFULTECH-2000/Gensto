30e7fd1b9a2c58967ff4b90bb6648efb13c96174
saifultech <saifultech.official@gmail.com>
2022-07-13 19:31:20 +0800

    test

--- app/views/Quotation/Create.php
+++ app/views/Quotation/Create.php
@@ -4,15 +4,18 @@
     <h1>Create Quotation</h1>
     <hr>
     <form action="<?=baseurl?>Quotation\Create" method="post">
+    
+   
+    <select class="form-select" id="customerid" name="customerid">
     <?php 
   
     while($key=$data['result']->fetch_assoc())
     {
     ?>
-    <select class="form-select" id="customerid" name="customerid">
     <option value="<?=$key['id']?>"><?=$key['nama']?></option>
-    </select>       
     <?php } ?>
+    </select>       
+   
     <label for="date">Date</label>
     <input type="date" name="date" value="date">    <br>
     <label for="Due date">Due Date</label>
